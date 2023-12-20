cd2e1d2da21d7e85a94f3a24983d4f335d354fef
Deepanshu Jain <152912677+Deepanshu28071998@users.noreply.github.com>
2023-12-20 21:49:00 +0530

    user input list and tuple

---
+++ user gave a list or tuple.py
@@ -0,0 +1,30 @@
+'''
+a=input().split(',')
+print(a)
+'''
+'''
+d=[1,2,4,5,8,9,12,10,15]
+print(d[0:7:2])
+'''
+
+'''
+a=input().split(' , ')
+print(a)
+'''
+'''
+import math
+l=[1,2,3,4,5]
+
+facts=[math.factorial(i)for i in l]
+print(facts)
+'''
+
+'''
+from math import*
+print(dir())
+l=[1,2,3,4,5]
+
+facts=[factorial(i)for i in l]
+print(facts)
+'''
+
